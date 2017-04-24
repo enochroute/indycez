088fb4e8dc02c15d93a4229581f1b4ef7c3414f1
José Martín Gamboa Murillo <martinsillo.gamboa@gmail.com>
2017-04-24 09:48:09 -0500

    se eliminan los comentarios de inicio

--- database/indycez.sql
+++ database/indycez.sql
@@ -1,29 +1,6 @@
-CREATE DATABASE  IF NOT EXISTS `indyce` /*!40100 DEFAULT CHARACTER SET utf8 */;
+CREATE DATABASE  IF NOT EXISTS `indyce`
 USE `indyce`;
--- MySQL dump 10.13  Distrib 5.7.17, for macos10.12 (x86_64)
---
--- Host: localhost    Database: indyce
--- ------------------------------------------------------
--- Server version	5.7.17
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
---
--- Table structure for table `datos_estadisticos`
---
-
 DROP TABLE IF EXISTS `datos_estadisticos`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `datos_estadisticos` (
   `id_dato` smallint(6) NOT NULL AUTO_INCREMENT,
   `nombre` varchar(128) DEFAULT NULL,
@@ -31,11 +8,6 @@ CREATE TABLE `datos_estadisticos` (
   `valor` varchar(128) DEFAULT NULL,
   PRIMARY KEY (`id_dato`)
 ) ENGINE=InnoDB AUTO_INCREMENT=86 DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `datos_estadisticos`
---
 
 LOCK TABLES `datos_estadisticos` WRITE;
 /*!40000 ALTER TABLE `datos_estadisticos` DISABLE KEYS */;
