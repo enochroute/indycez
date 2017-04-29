c7c7f044f054c395434208bd573b7919d074f643
Paco de la Rosa <paco_de_la_rosa@hotmail.com>
2017-04-29 00:20:59 -0500

    Se hace ajuste entre versiones

--- php/conexion.php
+++ php/conexion.php
@@ -3,11 +3,6 @@ $host="localhost";
 $user="midezuser";
 $pass="zac5*s_61B.D1";
 $nbd="indyce";
-<<<<<<< HEAD
-
-
-=======
->>>>>>> 350b90504639aa2c46318958a7341f3228ce91ad
 $con=mysqli_connect("localhost","$user","$pass","$nbd") or die ("No se pudo conectar con el servidor");
 if (!mysqli_set_charset($con, "utf8")) {
     exit();
