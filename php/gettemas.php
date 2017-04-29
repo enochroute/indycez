36ba28d73dc97bc4fe9604623b435f0134a32a4b
Paco de la Rosa <paco_de_la_rosa@hotmail.com>
2017-04-29 00:26:17 -0500

    Se hace ajuste entre versiones

--- php/gettemas.php
+++ php/gettemas.php
@@ -1,14 +1,8 @@
 <?php
 include 'conexion.php';
-<<<<<<< HEAD
-
 
 $query="SELECT * FROM temas_interes";
 
-
-=======
-$query="select * from temas_interes";
->>>>>>> 350b90504639aa2c46318958a7341f3228ce91ad
 $resultado=mysqli_query($con,$query);
 $datos=array();
 while($row=$resultado->fetch_assoc()){
