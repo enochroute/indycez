a7888105f882655d64514c0212b615111df70a4b
José Martín Gamboa Murillo <martinsillo.gamboa@gmail.com>
2017-05-25 13:52:08 -0500

    new file

---
+++ admin/class/catalogos.php
@@ -0,0 +1,40 @@
+<?php
+session_start();
+if($_SESSION['key'] != md5("labor vincit omnia")){
+    unset($_SESSION);
+    session_destroy();
+    header("Location:index.php");
+    die();
+}
+
+date_default_timezone_set('America/Mexico_City');
+
+class catalogos {
+
+    function nombre_region($v){
+         include("conexion.php");
+        $conn = new conexion();
+        $conexion = $conn->conectar();
+        $conexion->set_charset("utf8");
+        $Query = 'SELECT  r.id_region,r.nombre  FROM regiones r inner join municipios m on(r.id_region = m.id_region) where m.id_municipio = '.$v;
+        $ExQuery = $conexion->query($Query) or die($conexion->error);
+        $Res = $ExQuery->fetch_array(MYSQLI_NUM);
+        unset($ExQuery);
+        unset($Query);
+        $conexion->close();
+        unset($conexion);
+        unset($conn);
+        return $Res;
+    }
+
+}
+
+$catalogo = new catalogos();
+
+switch($_POST['accion']){
+    case "nombre_region":
+        $data = $catalogo->nombre_region($_POST['municipio']);
+    break;
+}
+
+echo json_encode($data);
